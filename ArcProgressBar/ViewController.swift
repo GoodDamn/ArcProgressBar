b9280d323c7e2e9bea602923644b221c4e7571c0
Grigory <grigorydum@gmail.com>
2022-04-29 04:41:11 -0700

    Initial Commit

---
+++ ArcProgressBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArcProgressBar
+//
+//  Created by Cell on 29.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
