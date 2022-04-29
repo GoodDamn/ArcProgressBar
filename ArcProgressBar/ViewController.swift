3f6f6f9671b6ca93011d088ec2325dad4d53636d
Grigory <grigorydum@gmail.com>
2022-04-29 11:37:57 -0700

    Added new example

--- ArcProgressBar/ViewController.swift
+++ ArcProgressBar/ViewController.swift
@@ -15,13 +15,13 @@ class ViewController: UIViewController {
         super.viewDidLoad();
         arcProgressBar.backgroundColor = .clear;
         
-        arcProgressBar.arcWidth = 15.0;
-        arcProgressBar.startAngle = 35.0;
-        arcProgressBar.endAngle = 180.0;
-        arcProgressBar.progress = 35.0;
+        arcProgressBar.arcWidth = 7.0;
+        arcProgressBar.startAngle = -180.0;
+        arcProgressBar.endAngle = 90.0;
+        arcProgressBar.progress = 135.0;
         arcProgressBar.maxProgress = 230.0;
-        arcProgressBar.arcBackground = .brown;
-        arcProgressBar.tintColor = .purple;
+        arcProgressBar.arcBackground = .gray;
+        arcProgressBar.tintColor = .green;
     }
 
 }
